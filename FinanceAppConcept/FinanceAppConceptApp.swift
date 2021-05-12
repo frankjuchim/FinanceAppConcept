3472e8ee21815cdea1471bf7249542888f7ebe09
Frank Juchim <frank@juchim.de>
2021-05-12 17:46:04 +0200

    Initial Commit

---
+++ FinanceAppConcept/FinanceAppConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceAppConceptApp.swift
+//  FinanceAppConcept
+//
+//  Created by Frank Juchim on 12.05.21.
+//
+
+import SwiftUI
+
+@main
+struct FinanceAppConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
